5512607402df257053a651b3aaadd67fc14ede8e
serii <seriiburduja@gmail.com>
2025-09-24 08:58:39 +0300

    feat: pars tittle

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import requests
+
+
 def main():
-    print("Hello, World!")
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+    link = "https://stpav.altuofianco.com/"
+    response = requests.get(link, headers=headers).text
+    soup = BeautifulSoup(response, 'lxml')
+    title = soup.title.text
+    print(f'{title}: title')
 
 
 if __name__ == "__main__":
