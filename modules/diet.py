31a7641567a16d72c34ff2e2339f127de7ad397c
serii <seriiburduja@gmail.com>
2025-09-25 09:08:46 +0300

    feat: categories to files

--- modules/diet.py
+++ modules/diet.py
@@ -1,12 +1,56 @@
+import os
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 def diet():
     # htmlToFile()
     soup = getSoupFromFile("index.html")
-    print(f"soup: {soup}")
+    links = getLinks(soup)
+    saveToJson(links, "all_categories.json")
+    all_categories = getDataFromJson("all_categories.json")
+    categoriesToFiles(all_categories)
+
+
+def categoriesToFiles(categories):
+    os.makedirs('pages', exist_ok=True)
+    count = 1
+    for category_name, category_link in categories.items():
+        if count < 3:
+            filename = replaceByArray(
+                category_name, [",", " ", "-", "'"]) + ".html"
+            filename = f"pages/{count}_{filename}"
+            text = getHtmlFromSite(category_link)
+            writeToFile(filename, text)
+        count += 1
+
+
+def replaceByArray(text, arr):
+    for item in arr:
+        text = text.replace(item, "_")
+    return text
+
+
+def getDataFromJson(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def saveToJson(data, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def getLinks(soup):
+    allProducts = soup.select(".mzr-tc-group-item-href")
+    all_categories = {}
+    for item in allProducts:
+        item_text = item.text
+        item_href = "https://health-diet.ru" + item.get("href")
+        all_categories[item_text] = item_href
+    return all_categories
 
 
 def getSoupFromFile(filename):
--- modules/index.py
+++ modules/index.py
@@ -1,18 +1,19 @@
-from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
-import requests
+# from fake_useragent import UserAgent
+# from bs4 import BeautifulSoup
+# import requests
+
 
 def indexPage():
-    session = requests.Session()
-
-    ua = UserAgent()
-    headers = {"User-Agent": ua.random}
-    link = "http://lc-progeo.local/wp-login.php"
-    data = {"log": "test", "pwd": "test"}
-    response = session.post(link, data=data, headers=headers).text
-    print(f"{response}: response")
-
-    pages_link = "http://lc-progeo.local/wp-admin/edit.php?post_type=page"
-    response = session.get(pages_link, headers=headers).text
-    soup = BeautifulSoup()
-    wp_heading_inline = 
+    pass
+    # session = requests.Session()
+    #
+    # ua = UserAgent()
+    # headers = {"User-Agent": ua.random}
+    # link = "http://lc-progeo.local/wp-login.php"
+    # data = {"log": "test", "pwd": "test"}
+    # response = session.post(link, data=data, headers=headers).text
+    # print(f"{response}: response")
+    #
+    # pages_link = "http://lc-progeo.local/wp-admin/edit.php?post_type=page"
+    # response = session.get(pages_link, headers=headers).text
+    # soup = BeautifulSoup()
