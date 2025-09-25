ae47fac386a189758ba498378cc78db39cf31dd1
serii <seriiburduja@gmail.com>
2025-09-25 22:46:30 +0300

    feat: menu

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from classes.Select import Select
 from modules.get_seo import get_seo
 from modules.get_soup_after_download import get_soup_after_download
 from modules.get_url import get_url
@@ -8,8 +9,21 @@ def main():
     url = get_url()
     if url:
         soup = get_soup_after_download(url)
+        menu(soup)
+
+
+def menu(soup):
+    menu_items = ["Check Images", "Check SEO", "Exit"]
+    selected = Select.select_one(menu_items)
+    if selected == "Check Images":
         images_has_alt(soup)
+        menu(soup)
+    elif selected == "Check SEO":
         get_seo(soup)
+        menu(soup)
+    elif selected == "Exit":
+        print("Exiting...")
+        exit()
 
 
 if __name__ == "__main__":
