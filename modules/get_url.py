947f547c8d8bd041a20d15056fcf565a195f942f
serii burduja <seriiburduja@gmail.com>
2025-09-26 12:59:38 +0300

    feat: get_url update

--- modules/get_url.py
+++ modules/get_url.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from classes.Clipboard import Clipboard
 from classes.PathHelper import PathHelper
 from classes.Print import Print
@@ -6,30 +7,31 @@ from classes.Select import Select
 
 def get_url():
     url = ""
-    options = ['Select saved urls', 'Choose url from clipboard']
+    options = ["Select saved urls", "Choose url from clipboard"]
     select_url = Select.select_with_fzf(options)
     ph = PathHelper()
-    script_dir = ph.script_dir.parent   # ✅ works
-    url_file = script_dir / 'urls.txt'
-    if select_url == ['Choose url from clipboard']:
-        clipboard = Clipboard.paste()
-        if not clipboard.startswith('http') or not clipboard.startswith('https'):
+    script_dir = ph.script_dir.parent  # ✅ works
+    url_file = script_dir / "urls.txt"
+    if select_url == ["Choose url from clipboard"]:
+        clipboard = (Clipboard.paste() or "").strip()
+        print(f"clipboard: {clipboard}")
+
+        if not is_http_url(clipboard):
             Print.error("Clipboard does not contain a valid URL.")
-            return
-        else:
-            url = clipboard
-            print("Do you want to save this URL? (y/n): ", end="")
-            if input().lower() == 'y':
-                with open(url_file, 'a') as f:
-                    f.write(url + '\n')
-                Print.success(f"URL saved to {url_file}")
-            return url
+            return None
+        url = clipboard
+        print("Do you want to save this URL? (y/n): ", end="")
+        if input().lower() == "y":
+            with open(url_file, "a") as f:
+                f.write(url + "\n")
+            Print.success(f"URL saved to {url_file}")
+        return url
     else:
         url_file = url_file
         if not url_file.exists():
             Print.error(f"No urls.txt file found at {url_file}")
             return
-        with open(url_file, 'r') as f:
+        with open(url_file, "r") as f:
             urls = [line.strip() for line in f if line.strip()]
         selected_url = Select.select_with_fzf(urls)
         if selected_url:
@@ -38,3 +40,12 @@ def get_url():
         else:
             Print.error("No URL selected.")
             return
+
+
+def is_http_url(s: str) -> bool:
+    s = (s or "").strip()
+    try:
+        u = urlparse(s)
+        return u.scheme in ("http", "https") and bool(u.netloc)
+    except ValueError:
+        return False
